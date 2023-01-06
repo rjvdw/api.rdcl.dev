9975a789b19890363bfd0c68449fa94fe741ed04
Ruud van der Weide <rj@rdcl.dev>
2023-01-06 23:38:42 +0100

    Improve errors to the user

---
+++ src/test/java/dev/rdcl/www/api/fixtures/Identities.java
@@ -0,0 +1,20 @@
+package dev.rdcl.www.api.fixtures;
+
+import dev.rdcl.www.api.auth.entities.Identity;
+
+import java.util.UUID;
+
+public class Identities {
+
+    public static Identity VALID_IDENTITY = Identity.builder()
+        .id(UUID.fromString("f277b076-f061-403c-bf7b-266eab926677"))
+        .name("John Doe")
+        .email("john.doe@example.com")
+        .build();
+
+    public static Identity INVALID_IDENTITY = Identity.builder()
+        .id(UUID.fromString("957621e8-f97c-410d-a0b8-80f72427afd4"))
+        .name("Jane Doe")
+        .email("jane.doe@example.com")
+        .build();
+}
