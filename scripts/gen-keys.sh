2cb34ba3f1fb43c337d3734f8d81d7ef13e110e1
Ruud van der Weide <rj@rdcl.dev>
2023-11-12 10:19:20 +0100

    Rename key files for signing JWTs

--- scripts/gen-keys.sh
+++ scripts/gen-keys.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-openssl genpkey -algorithm RSA -out src/main/resources/private-key.pem -pkeyopt rsa_keygen_bits:4096
-openssl rsa -in src/main/resources/private-key.pem -pubout -out src/main/resources/public-key.pem
+openssl genpkey -algorithm RSA -out src/main/resources/jwt.key -pkeyopt rsa_keygen_bits:4096
+openssl rsa -in src/main/resources/jwt.key -pubout -out src/main/resources/jwt.pem
